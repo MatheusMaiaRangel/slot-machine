5992cdab5ede25a43dbb0652335acd9efd6bf131
matheusmaiarangel <matheusmaiarangel@gmail.com>
2026-03-12 09:09:47 -0300

    Defini o método CarregarCreditos

--- Models/Jogo.cs
+++ Models/Jogo.cs
@@ -1,6 +1,31 @@
-﻿namespace slot_machine;
+﻿namespace slot_machine.Models;
 
 public class Jogo
 {
+    private float credito = 0;
+
+
+    public void Jogar()
+    {
+        
+    }    
     
+
+    public void CarregarCreditos()
+    {
+        Console.WriteLine($"Você possui {credito} créditos\nDigite quantos reais você quer depositar (Digite '0' para voltar): ");
+        credito += Convert.ToSingle(Console.ReadLine());
+    }
+
+    public void Regras()
+    {
+        
+    }
+
+    public void Estatisticas()
+    {
+        
+    }
+
 }
+
